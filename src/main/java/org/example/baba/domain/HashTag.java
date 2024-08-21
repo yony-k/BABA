22bc071d9fb42f19d98cab5a80f1bf399c2a38cd
Hajin <qwertygoov@naver.com>
2024-08-21 23:06:45 +0900

    [FEAT] HashTag Entity 정의

---
+++ src/main/java/org/example/baba/domain/HashTag.java
@@ -0,0 +1,22 @@
+package org.example.baba.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HashTag {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String tagName;
+}
