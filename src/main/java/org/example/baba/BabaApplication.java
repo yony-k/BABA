a896297b94077ca0e4a563b3f92ae7a1e77048b5
sung-silver <jinna0319@gmail.com>
2024-08-21 17:54:47 +0900

    [INIT] 프로젝트 초기 셋팅

---
+++ src/main/java/org/example/baba/BabaApplication.java
@@ -0,0 +1,13 @@
+package org.example.baba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabaApplication.class, args);
+    }
+
+}
