50e47ccb9eba7cca16da19847f32476cd603f8c9
sung-silver <jinna0319@gmail.com>
2024-08-21 20:17:23 +0900

    [FEAT] ExceptionType 인터페이스 정의

---
+++ src/main/java/org/example/baba/exception/exceptionType/ExceptionType.java
@@ -0,0 +1,13 @@
+package org.example.baba.exception.exceptionType;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionType {
+	HttpStatus status();
+
+	String message();
+
+	default int getHttpStatusCode() {
+		return status().value();
+	}
+}
