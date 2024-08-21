0a8f0a2570eca0c67d67c814d1f84692cfd7a6b5
sung-silver <jinna0319@gmail.com>
2024-08-21 20:17:40 +0900

    [FEAT] CustomException 구현

---
+++ src/main/java/org/example/baba/exception/CustomException.java
@@ -0,0 +1,15 @@
+package org.example.baba.exception;
+
+import org.example.baba.exception.exceptionType.ExceptionType;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+	private final ExceptionType exceptionType;
+
+	public CustomException(ExceptionType exceptionType) {
+		super(exceptionType.message());
+		this.exceptionType = exceptionType;
+	}
+}
