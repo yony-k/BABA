c20f400dceacffba673f724d0ca7e1b002d90e36
SeoJeongYu <bbwest0709@gmail.com>
2024-08-22 21:56:45 +0900

    [FEAT] 인증을 위한 UserDetailsService 구현

---
+++ src/main/java/org/example/baba/common/security/details/MemberDetailService.java
@@ -0,0 +1,31 @@
+package org.example.baba.common.security.details;
+
+import org.example.baba.domain.Member;
+import org.example.baba.exception.CustomException;
+import org.example.baba.exception.exceptionType.AuthorizedExceptionType;
+import org.example.baba.repository.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberDetailService implements UserDetailsService {
+
+  private final MemberRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String memberName) {
+
+    Member member =
+        repository
+            .findByEmail(memberName)
+            .orElseThrow(() -> new CustomException(AuthorizedExceptionType.UNAUTHENTICATED));
+
+    return new AuthUser(member);
+  }
+}
--- src/main/java/org/example/baba/repository/MemberRepository.java
+++ src/main/java/org/example/baba/repository/MemberRepository.java
@@ -1,6 +1,10 @@
 package org.example.baba.repository;
 
+import java.util.Optional;
+
 import org.example.baba.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {}
+public interface MemberRepository extends JpaRepository<Member, Long> {
+  Optional<Member> findByEmail(String email);
+}
