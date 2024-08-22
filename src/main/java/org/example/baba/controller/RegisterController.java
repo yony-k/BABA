6b576cd01350b519ca4cfdeebae7c610bcbe7079
YeonHee Kim <kyh03179@gmail.com>
2024-08-23 00:00:43 +0900

    [FEAT] RegisterController, RegisterService 생성

---
+++ src/main/java/org/example/baba/controller/RegisterController.java
@@ -0,0 +1,13 @@
+package org.example.baba.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/register")
+@RequiredArgsConstructor
+public class RegisterController {}
---
+++ src/main/java/org/example/baba/service/RegisterService.java
@@ -0,0 +1,9 @@
+package org.example.baba.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterService {}
