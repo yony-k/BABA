3ba950aa0793de2d9a0b583b939d86c38db1e6fb
YeonHee Kim <kyh03179@gmail.com>
2024-08-22 22:53:45 +0900

    [TEST] 10자 미만 실패 케이스 테스트 통과

--- src/test/java/org/example/baba/common/ValidAnotaionTest.java
+++ src/test/java/org/example/baba/common/ValidAnotaionTest.java
@@ -43,4 +43,19 @@ public class ValidAnotaionTest {
     // then
     assertTrue(result.isEmpty(), "비밀번호 유효성 검사가 실패했습니다.");
   }
+
+  @Test
+  @DisplayName("10자 이하인 비밀번호")
+  public void inValidPasswordToShort() {
+    // given
+    RegisterDTO registerDTO = new RegisterDTO("smith12", "Valid123!", "smith12@gmail.com");
+
+    // when
+    Set<ConstraintViolation<RegisterDTO>> result = validator.validate(registerDTO);
+
+    // then
+    for (ConstraintViolation<RegisterDTO> violation : result) {
+      assertTrue(violation.getMessage().contains("비밀번호는 10자 이상이어야합니다."), "메세지가 포함되어야합니다.");
+    }
+  }
 }
