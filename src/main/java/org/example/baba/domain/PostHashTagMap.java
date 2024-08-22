fe307d83a8734ea63482cb36e7fcf8e707754f7c
Hajin <qwertygoov@naver.com>
2024-08-22 10:01:11 +0900

    [FEAT] PostHashTagMap Entity 정의

---
+++ src/main/java/org/example/baba/domain/PostHashTagMap.java
@@ -0,0 +1,27 @@
+package org.example.baba.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostHashTagMap {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id")
+    private HashTag hashtag;
+
+}
