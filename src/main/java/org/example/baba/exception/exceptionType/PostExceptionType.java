f167ee00d79c78539f5985d8e2748e8a276e6492
Hajin <qwertygoov@naver.com>
2024-08-23 11:16:55 +0900

    [FEAT] 게시글 좋아요, 게시글 공유에서 외부 API 호출 성공 시, 로직 수행하도록 기능 추가

--- src/main/java/org/example/baba/exception/exceptionType/PostExceptionType.java
+++ src/main/java/org/example/baba/exception/exceptionType/PostExceptionType.java
@@ -11,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 public enum PostExceptionType implements ExceptionType {
 
   // 404 Not Found
-  NOT_FOUND_POST(HttpStatus.NOT_FOUND, "요청하신 게시글을 찾을 수 없습니다");
+  NOT_FOUND_POST(HttpStatus.NOT_FOUND, "요청하신 게시글을 찾을 수 없습니다"),
+  API_CALL_FAILED(HttpStatus.BAD_GATEWAY, "호출하신 API 가 응답을 주지 않거나 에러를 반환하였습니다."),
+  UNSUPPORTED_SNS(HttpStatus.BAD_REQUEST, "지원하지 않는 SNS 입니다.");
 
   private final HttpStatus status;
   private final String message;
--- src/main/java/org/example/baba/service/PostService.java
+++ src/main/java/org/example/baba/service/PostService.java
@@ -1,36 +1,92 @@
 package org.example.baba.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.example.baba.domain.Post;
 import org.example.baba.domain.enums.SNSType;
 import org.example.baba.exception.CustomException;
 import org.example.baba.exception.exceptionType.PostExceptionType;
 import org.example.baba.repository.PostRepository;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
+  private final WebClient webClient;
   private final PostRepository postRepository;
 
   @Transactional
   public void likePost(Long postId, SNSType type) {
-    Post post =
-        postRepository
-            .findByIdAndType(postId, type)
-            .orElseThrow(() -> new CustomException(PostExceptionType.NOT_FOUND_POST));
-    post.like();
+    String uri = getUriForSNSType(postId, type, "likes");
+    callApiProcess(
+        HttpMethod.POST,
+        uri,
+        () -> {
+          Post post =
+              postRepository
+                  .findByIdAndType(postId, type)
+                  .orElseThrow(() -> new CustomException(PostExceptionType.NOT_FOUND_POST));
+          post.like();
+        });
   }
 
   @Transactional
   public void sharePost(Long postId, SNSType type) {
-    Post post =
-        postRepository
-            .findByIdAndType(postId, type)
-            .orElseThrow(() -> new CustomException(PostExceptionType.NOT_FOUND_POST));
-    post.share();
+    String uri = getUriForSNSType(postId, type, "share");
+    callApiProcess(
+        HttpMethod.POST,
+        uri,
+        () -> {
+          Post post =
+              postRepository
+                  .findByIdAndType(postId, type)
+                  .orElseThrow(() -> new CustomException(PostExceptionType.NOT_FOUND_POST));
+          post.share();
+        });
+  }
+
+  private String getUriForSNSType(Long postId, SNSType type, String action) {
+    // SNS 타입에 따라 URI 매핑
+    Map<SNSType, String> uriMap = new HashMap<>();
+    uriMap.put(SNSType.FACEBOOK, "https://www.facebook.com/" + action + "/" + postId);
+    uriMap.put(SNSType.TWITTER, "https://www.twitter.com/" + action + "/" + postId);
+    uriMap.put(SNSType.INSTAGRAM, "https://www.instagram.com/" + action + "/" + postId);
+    uriMap.put(SNSType.THREADS, "https://www.threads.com/" + action + "/" + postId);
+
+    // 위의 SNS 가 아닐 경우, 예외 발생
+    String uri = uriMap.get(type);
+    if (uri == null) {
+      throw new CustomException(PostExceptionType.UNSUPPORTED_SNS);
+    }
+
+    return uri;
+  }
+
+  private void callApiProcess(HttpMethod method, String uri, Runnable onSuccess) {
+    WebClient.RequestHeadersSpec<?> requestHeadersSpec = webClient.method(method).uri(uri);
+
+    requestHeadersSpec
+        .retrieve()
+        .toBodilessEntity()
+        .doOnSuccess(
+            response -> {
+              log.info("Success to call API - URI : " + uri);
+              onSuccess.run();
+            })
+        .doOnError(
+            error -> {
+              log.error("Failed to call API");
+              throw new CustomException(PostExceptionType.API_CALL_FAILED);
+            })
+        .block();
   }
 }
