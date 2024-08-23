fedcac4f49c34588d20ebc32967df7c1d27bce10
Sona <objet_an@naver.com>
2024-08-23 22:04:53 +0900

    [FEAT]상세조회 내 해시태그 추가(DTO,SERVCIE)

--- src/main/java/org/example/baba/controller/dto/response/PostDetailResponseDto.java
+++ src/main/java/org/example/baba/controller/dto/response/PostDetailResponseDto.java
@@ -1,6 +1,7 @@
 package org.example.baba.controller.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.example.baba.domain.Post;
 import org.example.baba.domain.enums.SNSType;
@@ -21,19 +22,21 @@ public class PostDetailResponseDto {
   private SNSType type;
   private String title;
   private String content;
+  private List<String> hashtags;
   private int likeCount;
   private int shareCount;
   private int viewCount;
   private LocalDateTime createdAt;
   private LocalDateTime updatedAt;
 
-  public static PostDetailResponseDto from(Post post) {
+  public static PostDetailResponseDto from(Post post, List<String> postHashtags) {
     return PostDetailResponseDto.builder()
         .id(post.getId())
         .memberId(post.getMemberId())
         .type(post.getType())
         .title(post.getTitle())
         .content(post.getContent())
+        .hashtags(postHashtags)
         .likeCount(post.getLikeCount())
         .shareCount(post.getShareCount())
         .viewCount(post.getViewCount())
