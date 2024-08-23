34dc35feea7692ab75a052071ddb89afeb7dfe42
YeonHee Kim <kyh03179@gmail.com>
2024-08-23 09:50:03 +0900

    [FEAT] MemberService isDuplicated 메소드 생성

--- src/main/java/org/example/baba/repository/MemberRepository.java
+++ src/main/java/org/example/baba/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
   Optional<Member> findByEmail(String email);
 
-  boolean existsByMembername(String membername);
+  boolean existsByMemberName(String memberName);
 
   boolean existsByEmail(String email);
 }
