39412d78c02d2407d08d23dff94cad9c43ba81bb
sung-silver <jinna0319@gmail.com>
2024-08-24 19:02:39 +0900

    [FEAT] 구현 로직 변경

--- src/main/java/org/example/baba/repository/StatisticsRepository.java
+++ src/main/java/org/example/baba/repository/StatisticsRepository.java
@@ -1,15 +1,13 @@
 package org.example.baba.repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.example.baba.domain.Post;
-import org.example.baba.domain.QHashTag;
 import org.example.baba.domain.QPost;
-import org.example.baba.domain.QPostHashTagMap;
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -17,25 +15,53 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class StatisticsRepository {
+
   private final JPAQueryFactory queryFactory;
 
-  public List<Post> findPosts(String hashtag, LocalDate start, LocalDate end) {
+  // 날짜별로 그룹핑하여 데이터 조회
+  public List<Tuple> findPostsGroupedByDate(String hashtag, LocalDate start, LocalDate end) {
     QPost post = QPost.post;
-    QPostHashTagMap postHashTagMap = QPostHashTagMap.postHashTagMap;
-    QHashTag hashTag = QHashTag.hashTag;
-
-    BooleanExpression hashtagCondition = hashTag.tagName.eq(hashtag);
-    BooleanExpression dateCondition =
-        post.createdAt.between(start.atStartOfDay(), end.atTime(23, 59, 59));
 
     return queryFactory
-        .selectDistinct(post)
+        .select(
+            post.createdAt.year(),
+            post.createdAt.month(),
+            post.createdAt.dayOfMonth(),
+            post.viewCount.sum(),
+            post.likeCount.sum(),
+            post.shareCount.sum(),
+            post.id.count())
         .from(post)
-        .leftJoin(postHashTagMap)
-        .on(post.id.eq(postHashTagMap.post.id))
-        .leftJoin(hashTag)
-        .on(postHashTagMap.hashtag.id.eq(hashTag.id))
-        .where(hashtagCondition.and(dateCondition))
+        .where(
+            post.createdAt.between(start.atStartOfDay(), end.atTime(23, 59)),
+            post.postHashTags.any().hashtag.tagName.eq(hashtag))
+        .groupBy(post.createdAt.year(), post.createdAt.month(), post.createdAt.dayOfMonth())
+        .fetch();
+  }
+
+  // 시간별로 그룹핑하여 데이터 조회
+  public List<Tuple> findPostsGroupedByHour(
+      String hashtag, LocalDateTime start, LocalDateTime end) {
+    QPost post = QPost.post;
+
+    return queryFactory
+        .select(
+            post.createdAt.year(),
+            post.createdAt.month(),
+            post.createdAt.dayOfMonth(),
+            post.createdAt.hour(),
+            post.viewCount.sum(),
+            post.likeCount.sum(),
+            post.shareCount.sum(),
+            post.id.count())
+        .from(post)
+        .where(
+            post.createdAt.between(start, end), post.postHashTags.any().hashtag.tagName.eq(hashtag))
+        .groupBy(
+            post.createdAt.year(),
+            post.createdAt.month(),
+            post.createdAt.dayOfMonth(),
+            post.createdAt.hour())
         .fetch();
   }
 }
--- src/main/java/org/example/baba/service/StatisticsService.java
+++ src/main/java/org/example/baba/service/StatisticsService.java
@@ -2,50 +2,51 @@ package org.example.baba.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.example.baba.common.enums.StatisticsType;
 import org.example.baba.common.enums.StatisticsValue;
-import org.example.baba.domain.Post;
 import org.example.baba.repository.StatisticsRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.core.Tuple;
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-@Slf4j
 public class StatisticsService {
 
   private final StatisticsRepository statisticsRepository;
 
-  public Map<String, Integer> getStatistics(
+  public Map<String, Long> getStatistics(
       final String hashtag,
       final StatisticsType type,
       final StatisticsValue value,
       final LocalDate start,
       final LocalDate end) {
-    Map<String, Integer> result;
+
+    Map<String, Long> result;
     String hashTag = getOrDefaultEndDate(hashtag);
+    List<Tuple> posts;
 
-    List<Post> posts = statisticsRepository.findPosts(hashTag, start, end);
-
-    switch (type) {
-      case DATE:
-        result = groupByDate(posts, start, end, value);
-        break;
-      case HOUR:
-        result = groupByHour(posts, start, end, value);
-        break;
-      default:
-        throw new IllegalArgumentException("Invalid type parameter");
+    if (type == StatisticsType.DATE) {
+      posts = statisticsRepository.findPostsGroupedByDate(hashTag, start, end);
+      result = groupByDate(posts, value, start, end);
+    } else if (type == StatisticsType.HOUR) {
+      LocalDateTime startDateTime = start.atStartOfDay();
+      LocalDateTime endDateTime = end.atTime(23, 59);
+      posts = statisticsRepository.findPostsGroupedByHour(hashTag, startDateTime, endDateTime);
+      result = groupByHour(posts, value, startDateTime, endDateTime);
+    } else {
+      throw new IllegalArgumentException("Invalid type parameter");
     }
 
     return result;
@@ -59,57 +60,88 @@ public class StatisticsService {
     return hashtag;
   }
 
-  private Map<String, Integer> groupByDate(
-      List<Post> posts, LocalDate start, LocalDate end, StatisticsValue value) {
-    Map<String, Integer> result = new HashMap<>();
+  private Map<String, Long> groupByDate(
+      List<Tuple> results, StatisticsValue value, LocalDate start, LocalDate end) {
+    Map<String, Long> result = new TreeMap<>(); // TreeMap을 사용하여 날짜를 자동으로 정렬
 
-    for (LocalDate date = start; !date.isAfter(end); date = date.plusDays(1)) {
-      LocalDate finalDate = date;
-      int sum =
-          posts.stream()
-              .filter(post -> post.getCreatedAt().toLocalDate().isEqual(finalDate))
-              .mapToInt(post -> getValue(post, value))
-              .sum();
-      result.put(date.toString(), sum);
+    // Fill result map with default values
+    LocalDate current = start;
+    while (!current.isAfter(end)) {
+      result.put(current.toString(), 0L);
+      current = current.plusDays(1);
+    }
+
+    for (Tuple tuple : results) {
+      LocalDate date =
+          LocalDate.of(
+              tuple.get(0, Integer.class), // Year
+              tuple.get(1, Integer.class), // Month
+              tuple.get(2, Integer.class) // Day
+              );
+      String dateStr = date.toString();
+
+      long sumValue =
+          getValue(
+              tuple.get(3, Integer.class), // Sum of view count
+              tuple.get(4, Integer.class), // Sum of like count
+              tuple.get(5, Integer.class), // Sum of share count
+              tuple.get(6, Long.class), // Count of posts
+              value);
+      result.put(dateStr, sumValue);
     }
 
     return result;
   }
 
-  private Map<String, Integer> groupByHour(
-      List<Post> posts, LocalDate start, LocalDate end, StatisticsValue value) {
-    Map<String, Integer> result = new HashMap<>();
+  private Map<String, Long> groupByHour(
+      List<Tuple> results, StatisticsValue value, LocalDateTime start, LocalDateTime end) {
+    Map<String, Long> result = new TreeMap<>(); // TreeMap을 사용하여 시간을 자동으로 정렬
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:00");
 
-    for (LocalDateTime dateTime = start.atStartOfDay();
-        !dateTime.isAfter(end.atTime(23, 59));
-        dateTime = dateTime.plusHours(1)) {
-      LocalDateTime finalDateTime = dateTime;
-      int sum =
-          posts.stream()
-              .filter(
-                  post -> {
-                    LocalDateTime createdAt = post.getCreatedAt();
-                    return createdAt.isAfter(finalDateTime)
-                        && createdAt.isBefore(finalDateTime.plusHours(1));
-                  })
-              .mapToInt(post -> getValue(post, value))
-              .sum();
-      result.put(dateTime.toString(), sum);
+    // Fill result map with default values
+    LocalDateTime current = start;
+    while (!current.isAfter(end)) {
+      String formattedDateTime = current.format(formatter);
+      result.put(formattedDateTime, 0L);
+      current = current.plusHours(1);
+    }
+
+    for (Tuple tuple : results) {
+      String dateTimeStr =
+          String.format(
+              "%d-%02d-%02dT%02d:00",
+              tuple.get(0, Integer.class), // Year
+              tuple.get(1, Integer.class), // Month
+              tuple.get(2, Integer.class), // Day
+              tuple.get(3, Integer.class) // Hour
+              );
+
+      long sumValue =
+          getValue(
+              tuple.get(4, Integer.class), // Sum of view count
+              tuple.get(5, Integer.class), // Sum of like count
+              tuple.get(6, Integer.class), // Sum of share count
+              tuple.get(7, Long.class), // Count of posts
+              value);
+      result.put(dateTimeStr, sumValue);
     }
 
     return result;
   }
 
-  private int getValue(Post post, StatisticsValue value) {
+  private long getValue(
+      int viewCount, int likeCount, int shareCount, long postCount, StatisticsValue value) {
     switch (value) {
       case VIEW_COUNT:
-        return post.getViewCount();
+        return viewCount;
       case LIKE_COUNT:
-        return post.getLikeCount();
+        return likeCount;
       case SHARE_COUNT:
-        return post.getShareCount();
+        return shareCount;
+      case COUNT:
+        return postCount;
       default:
-        return 1;
+        return 0;
     }
   }
 }
